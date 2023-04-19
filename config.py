75bd104a68e7e7ab122022ec3da2f474abb39154
msi-JunXiang <jimmy871117@gmail.com>
2023-04-19 22:02:20 +0800

    add: grid image

--- config.py
+++ config.py
@@ -4,11 +4,11 @@ KEY_TO_STOP = 'ctrl+c'
 KEY_TO_SPLIT = 's'  # 新增一个快捷键用于分段
 
 # 定義輸入圖片路徑
-IMG_PATH = './image/四色.png'
+IMG_PATH = './image/heart.png'
 
 # 定義 RGB value 數量
 # 例如一張圖片60%紅色、30%藍色、10%白色，RGB_NUM=2，就萃取最大成分的紅、藍 (RGB_NUM=-1，全取)
-RGB_NUM = 4
+RGB_NUM = 2
 
 # 定義目標視窗 Title
 WINDOW_TITLE = 'X75 RGB Keyboard'
